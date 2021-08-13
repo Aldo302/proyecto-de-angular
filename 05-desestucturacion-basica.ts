b6ae32d3f5259471e097346ab58ba322625bbb01
Aldo 302 <aldosanz02@gmail.com>
2021-08-13 10:51:31 -0500

    primer commit

---
+++ 05-desestucturacion-basica.ts
@@ -0,0 +1,6 @@
+const dbz: string []= ['Goku', 'Vegeta', 'Trunks'];
+const [p1, p2, p3] = dbz;
+
+console.log('Personaje 1:', p1);
+console.log('Personaje 2:', p2);
+console.log('Personaje 3:', p3);
